dff1fd49c9e23f3b06b0dd2c5a57d19bd9cb7584
drewphillips <drew@superheroprinting.com>
2018-05-31 15:47:29 -0700

    stopping for lunch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -44,7 +44,7 @@ $(document).ready(function () {
     }
     function checker(guessed) {
   
-      if (guessed === game.trivia.answers[0]) {
+      if (guessed === game.trivia.answers[nextQuestion][0]) {
         alert("correct!");
       } else if (guessed !== game.trivia.answers[nextQuestion[0]]) {
         alert("incorrect");
@@ -76,17 +76,17 @@ $(document).ready(function () {
   
   
     //Shuffles Array of Questions and Answers
-    function shuffle(array) {
-      var i = 0
-        , j = 0
-        , temp = null
+   // function shuffle(array) {
+    //  var i = 0
+     //   , j = 0
+    //    , temp = null
   
-      for (i = array.length - 1; i > 0; i -= 1) {
-        j = Math.floor(Math.random() * (i + 1))
-        temp = array[i]
-        array[i] = array[j]
-        array[j] = temp
-      }
-      return array[j];
+     // for (i = array.length - 1; i > 0; i -= 1) {
+     //   j = Math.floor(Math.random() * (i + 1))
+     //   temp = array[i]
+     //   array[i] = array[j]
+     //   array[j] = temp
+     // }
+     // return array[j];
     }
   })
